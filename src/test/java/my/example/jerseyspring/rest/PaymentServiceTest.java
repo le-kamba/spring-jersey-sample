8bc8df5a0044e9381c0df1707b6029b914e25f09
les-kasa-1010 <les.kasa.1010@gmail.com>
2020-04-07 01:44:26 +0900

    Spring DIをいれてモック化テスト

--- src/test/java/my/example/jerseyspring/rest/PaymentServiceTest.java
+++ src/test/java/my/example/jerseyspring/rest/PaymentServiceTest.java
@@ -5,12 +5,17 @@ import org.glassfish.jersey.test.JerseyTest;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import javax.ws.rs.core.Application;
 import javax.ws.rs.core.Response;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration("classpath:/applicationContext.xml")
 class PaymentServiceTest extends JerseyTest {
 
     @Override
@@ -35,6 +40,6 @@ class PaymentServiceTest extends JerseyTest {
         final Response response = target("/payment/mkyong").request().get();
 
         String content = response.readEntity(String.class);
-        assertThat(content).isEqualTo("Jersey + Spring example");
+        assertThat(content).isEqualTo("This is mock.");
     }
 }
