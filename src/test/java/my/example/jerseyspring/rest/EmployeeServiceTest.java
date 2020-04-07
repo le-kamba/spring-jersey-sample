24923a909e23b7108787f13b3fb47b1c72358091
les-kasa-1010 <les.kasa.1010@gmail.com>
2020-04-07 22:13:03 +0900

    optimize imports

--- src/test/java/my/example/jerseyspring/rest/EmployeeServiceTest.java
+++ src/test/java/my/example/jerseyspring/rest/EmployeeServiceTest.java
@@ -15,8 +15,6 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -25,7 +23,6 @@ import javax.ws.rs.core.Application;
 import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-
 import java.util.List;
 import java.util.stream.Stream;
 
