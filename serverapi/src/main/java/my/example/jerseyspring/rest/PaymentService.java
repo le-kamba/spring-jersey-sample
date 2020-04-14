fcea8b91994ea1c23a4b6566f56c5745c603a583
les-kasa-1010 <les.kasa.1010@gmail.com>
2020-04-14 21:47:18 +0900

    base_url/サービス名でアクセスできるように対応(index.jspにはアクセスできなくなるため削除)

--- serverapi/src/main/java/my/example/jerseyspring/rest/PaymentService.java
+++ serverapi/src/main/java/my/example/jerseyspring/rest/PaymentService.java
@@ -2,12 +2,13 @@ package my.example.jerseyspring.rest;
 
 import my.example.jerseyspring.repository.transaction.TransactionBo;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
-@Component
+@Service
 @Path("/payment")
 public class PaymentService {
 
